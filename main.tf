16bb012d1127c4d6e967ab4a121df049784fc9f6
uclides gil <uclidesgil@gmail.com>
2019-09-27 19:44:54 -0300

    initial files

---
+++ main.tf
@@ -0,0 +1,136 @@
+terraform {
+  backend "gcs" {
+
+  }
+}
+
+#permission sa to bucket
+resource "google_storage_bucket_iam_binding" "binding" {
+  bucket  = "${var.bucket}"
+  role    = "${var.roles_bucket_iam_binding}"
+  members = "${var.users_bucket_iam_binding}"
+}
+
+#firewall
+resource "google_compute_firewall" "cf" {
+  name    = "${var.name_fw}"
+  network = "${var.vpc}"
+
+  allow {
+    protocol = "${var.protocol_fw}"
+    ports    = "${var.ports_fw}"
+  }
+  source_ranges = "${var.source_ranges_fw}"
+
+}
+
+//public ip
+resource "google_compute_address" "ip_public" {
+  provider     = "google-beta"
+  name         = "${var.name_ip_public}"
+  labels       = "${var.labels}"
+}
+
+#create VM
+resource "google_compute_instance" "google-instance-core" {
+  provider     = "google-beta"
+  count        = 1
+  name         = "${var.product}-${count.index + 1}-${var.region}-${var.environment}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.zone}"
+  project      = "${var.project}"
+
+  boot_disk {
+    initialize_params {
+      image  = "${var.image_vm}"
+      type   = "${var.type_disk_size_vm}"
+      size   = "${var.boot_disk_size_vm}"
+      labels = "${var.labels}"
+    }
+  }
+  network_interface {
+    subnetwork = "${var.subnet}"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+  /*metadata = {
+  startup-script-url = "path to script, example : bucket"
+  shutdown-script-url = "path to script, example : bucket"
+  }*/
+
+  service_account {
+    email  = "${var.service_account_email}"
+    scopes = "${var.scope_service_account}"
+  }
+  tags                      = "${var.tags_vm}"
+  labels                    = "${var.labels}"
+  allow_stopping_for_update = true
+}
+
+#create instance group and attach vm
+resource "google_compute_instance_group" "coreweb-instance-group" {
+  count       = 1
+  name        = "${var.instance_group_name}"
+  description = ""
+  instances   = [
+    "${element(google_compute_instance.google-instance-core.*.self_link, count.index)}",
+  ]
+
+  named_port {
+    name = "${var.name_port_instance_group}"
+    port = "${var.port_instance_group}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  zone = "${var.zone}"
+}
+
+#crear backend
+resource "google_compute_backend_service" "staging_service" {
+  count                   = 1
+  name                    = "${var.name_bs}"
+  port_name               = "${var.port_name_bs}"
+  protocol                = "${var.protocol_bs}"
+  load_balancing_scheme   = "${var.schema_bs}"
+  session_affinity        = "${var.session_affinity_bs}"
+  affinity_cookie_ttl_sec = "${var.ttl_bs}"
+  backend {
+    group = "${element(google_compute_instance_group.coreweb-instance-group.*.self_link, count.index)}"
+  }
+
+  health_checks = [
+    "${google_compute_health_check.health_check.self_link}",
+  ]
+}
+
+resource "google_compute_health_check" "health_check" {
+  provider = "google-beta"
+
+  name     = "${var.name_hc}"
+  http_health_check {
+    request_path       = "${var.path_hc}" 
+    port               = "${var.port_hc}"
+    port_name          = "${var.port_name_hc}"
+  }
+}
+
+resource "google_compute_global_forwarding_rule" "default" {
+  name       = "${var.name_fr}"
+  target     = "${google_compute_target_http_proxy.default.self_link}"
+  port_range = "${var.port_range_fr}"
+}
+
+resource "google_compute_target_http_proxy" "default" {
+  name        = "${var.name_tp}"
+  url_map     = "${google_compute_url_map.default.self_link}"
+}
+
+resource "google_compute_url_map" "default" {
+  name            = "${var.name_um}"
+  default_service = "${element(google_compute_backend_service.staging_service.*.self_link, 1)}"
+
+}
---
+++ vars.tf
@@ -0,0 +1,221 @@
+#general
+variable "vpc" {
+  description = "The vpc to deploy resources"
+  type        = string
+}
+
+variable "subnet" {
+  description = "The subnet to deploy resources"
+  type        = string
+}
+
+variable "project_id" {
+  description = "The project ID of google"
+  type        = string
+}
+
+variable "region" {
+  description = "The region of the resources"
+  type        = string
+}
+
+variable "zone" {
+  description = "The specified zone to resources, it should be something like: `a`, `c`."
+  type        = string
+}
+
+variable "product" {
+  description = "The name of the service as product"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment name of service"
+  type        = string
+}
+
+#VM
+variable "machine_type" {
+  description = "Type machine of vm"
+  type        = string
+}
+
+variable "image_vm" {
+  description = "Type image of vm"
+  type        = string
+}
+
+variable "boot_disk_size_vm" {
+  description = "Size of boot disk vm"
+  type        = number
+}
+
+variable "type_disk_size_vm" {
+  description = "Size of boot disk of vm"
+  type        = string
+}
+
+variable "metadata" {
+  description = "Map of metadata values to pass to instances."
+  type        = map
+}
+
+variable "tags_vm" {
+  description = "The tags for VM"
+  type        = list(string)
+}
+
+variable "labels" {
+  description = "labels to resources"
+  type        = map
+}
+
+
+#instance group
+variable "instance_group_name" {
+  description = "The name of instance group"
+  type        = string
+}
+
+variable "name_port_instance_group" {
+  description = "The name of port in instance group"
+  type        = string
+}
+
+variable "port_instance_group" {
+  description = "The ports in instance group"
+  type        = number
+}
+
+#bucket
+variable "bucket" {
+  description = "bucket of metadata VM"
+  type        = string
+  
+}
+
+variable "roles_bucket_iam_binding" {
+  description = "The permission of bucket"
+  type        = string
+  
+}
+
+variable "users_bucket_iam_binding" {
+  description = "The users to binding bucket"
+  type        = list(string)
+  
+}
+
+#backend service
+variable "name_bs" {
+  description = "The name of the backend service"
+  type        = string
+}
+
+variable "port_name_bs" {
+  description = "The name of the backend service"
+  type        = string
+}
+
+variable "protocol_bs" {
+  description = "The type protocol to backend service"
+  type        = string
+}
+
+variable "schema_bs" {
+  description = "The type schema to backend service"
+  type        = string
+}
+
+variable "session_affinity_bs" {
+  description = "The session affinity to backend service"
+  type        = string
+}
+
+variable "ttl_bs" {
+  description = "The ttl to backend service"
+  type        = number
+}
+
+#firewall
+variable "name_fw" {
+  description = "The name for the firewall rules"
+  type        = string
+}
+
+variable "protocol_fw" {
+  description = "The specified protocol to firewall rules"
+  type        = string
+}
+
+variable "ports_fw" {
+  description = "The list of ports for apply in firewall rules"
+  type        = list(string)
+}
+
+variable "source_ranges_fw" {
+  description = "ranges to allow access in firewall rules"
+  type        = list(string)
+}
+
+#health check
+variable "name_hc" {
+  description = "The name for the health check"
+  type        = string
+}
+
+variable "path_hc" {
+  description = "The path for the health check"
+  type        = string
+}
+
+variable "port_hc" {
+  description = "The port for the health check"
+  type        = string
+}
+
+variable "port_name_hc" {
+  description = "The port name for the health check"
+  type        = string
+}
+
+#forwarding rule
+variable "name_fr" {
+  description = "The name for the forwarding rule"
+  type        = string
+}
+
+variable "port_range_fr" {
+  description = "The port range for the forwarding rule"
+  type        = string
+}
+
+#target http proxy
+variable "name_tp" {
+  description = "The name for the target proxy"
+  type        = string
+}
+
+#url map
+variable "name_um" {
+  description = "The name for the url map"
+  type        = string
+}
+
+#ips
+variable "name_ip_public" {
+    description = "The name of ip public"
+    type = string
+}
+
+#service account
+variable "service_account_email" {
+    description = "The service account email to allow permision in resources"
+    type = string
+}
+
+variable "scope_service_account" {
+  description = "The list of scopes to service account"
+  type        = list(string)
+}
+
